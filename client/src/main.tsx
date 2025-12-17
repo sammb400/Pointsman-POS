3d6d1ca1b05cc57f761a39144a3873481bf1536b
Samuel Mwai <100771908+sammb400@users.noreply.github.com>
2025-12-17 11:33:11 +0300

    initialization

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-console.log("hello world");
+
 
 createRoot(document.getElementById("root")!).render(<App />);
